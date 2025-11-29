084d231811ab23c513e4ace3b792ccb6ec84fb35
Lydia Bagdon <74695306+lbagd9898@users.noreply.github.com>
2025-11-28 23:23:12 -0500

    validation function added, need to make signup method

--- server/app.js
+++ server/app.js
@@ -1,17 +1,17 @@
 import express from "express";
 import { prisma } from "./prismaClient.js";
-import cors from 'cors';
-import path from 'path';
-import authRouter from './router/authRouter.js'
+import cors from "cors";
+import path from "path";
+import authRouter from "./routers/authRouter.js";
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const PORT = 3000;
 
-app.use('/auth', authRouter)
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log("server running");
