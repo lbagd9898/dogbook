84312393cdf6dc5099e01c7d9b1a9d86e596b1ef
Lydia Bagdon <74695306+lbagd9898@users.noreply.github.com>
2025-12-24 12:40:15 -0500

    progress made designing dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,17 @@
 import Navbar from "../components/Navbar";
+import pawprint from "../assets/icons/pawprint.svg";
 import Rightsidebar from "../components/Rightsidebar";
 
 export default function Dashboard() {
   return (
     <div className="grid grid-cols-[16rem_1fr_14rem] min-h-screen">
       <Navbar />
-      <main className="bg-red-200">This is the main content</main>
+      <main className="p-6">
+        <div className="flex gap-2 items-center font-doggy">
+          <h1 className="text-lg md:text-xl lg:text-2xl">Your BarkFeed</h1>
+          <img className="w-[1.5em] h-[1.5em]" src={pawprint} alt="" />
+        </div>
+      </main>
       <Rightsidebar />
     </div>
   );
