4f928d74d5daabcf0e725889439cc43423852ad7
Lydia Bagdon <74695306+lbagd9898@users.noreply.github.com>
2026-02-09 06:37:00 -0500

    made makepost card now need to just make the page more responsive

--- client/src/components/Makepost.jsx
+++ client/src/components/Makepost.jsx
@@ -7,8 +7,20 @@ export default function Makepost() {
         <img src={wolfpack} className="h-[1em] w-[1em]" alt="" />
         <h1>Username</h1>
       </div>
-      <form action="">
-        <div className="bg-white rounded p-2 shadow-md">xxx</div>
+      <form classname="w-full" action="post">
+        <textarea
+          type="text"
+          placeholder="What's on your mind?"
+          className="w-full border-grey border bg-white rounded p-2 shadow-md"
+        />
+        <div className="w-full flex justify-end items-center">
+          <button
+            className="bg-[#99D49F] hover:bg-[#6DB77A] mt-2 px-3 py-1 rounded shadow text-lg"
+            action="submit"
+          >
+            Post
+          </button>
+        </div>
       </form>
     </div>
   );
